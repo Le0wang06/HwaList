a711f381c135bf1ff19f91145a99fc20dc9e37d8
Le0wang06 <098leowang@gmail.com>
2025-03-28 22:50:48 -0700

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
-
-
+import { Routes, Route } from 'react-router-dom'
+import Favorites from './pages/Favorites'
 
 function App() {
-  return <Home />
+  <main>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
+  </main>
 }
 
 export default App

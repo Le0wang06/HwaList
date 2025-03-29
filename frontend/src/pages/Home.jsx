54e4347b69269317570b8bdaf5b627ccf22876ac
Le0wang06 <098leowang@gmail.com>
2025-03-28 22:22:18 -0700

    fxed some error

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import MovieCard from "../components/MovieCard";
 
 function Home(){
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+
   const movies = [
     {id: 1, title: "Inception", release_date: "2010"},
     {id: 2, title: "Interstellar", release_date: "2014"},
@@ -12,18 +16,19 @@ function Home(){
 
   const handleSearch = () => {
 
-
-
   }
-
-
     return <div className="home">
       <form onSubmit={handleSearch} className="search-form">
         <input 
           type="text" 
           placeholder="Search for a movie" 
           className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
+        
+        <button type="submit" className="search-button">Search</button>
+
       </form>
       <div className="movie-grid">
         {movies.map((movie) => (
